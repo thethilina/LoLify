1dc9bd18f11b4ea59a3b872bd0d2c278c757bae6
Thilina Dewmina <thilinadewmina7@gmail.com>
2025-10-21 14:15:42 +0530

    user routs

---
+++ app/api/users/[search]/route.ts
@@ -0,0 +1,50 @@
+import { NextResponse } from "next/server";
+import connect from "@/lib/db";
+import User from "@/lib/models/users";
+import { request } from "http";
+import mongoose from "mongoose";
+import { Types } from "mongoose";
+
+export const GET = async (request : Request , context : {params :any} )=> {
+
+
+const searchText = await context.params.search;
+
+
+
+try{
+
+
+
+if (!searchText) {
+      return new NextResponse(
+        JSON.stringify({ message: "Search text shouldnt be empty " }),
+        { status: 400 }
+      );
+    }
+
+await connect();
+
+const results = await User.find({
+username : {$regex: searchText, $options: "i"}
+})
+
+if(!results){
+   return new NextResponse(
+        JSON.stringify({ message: "No users found" }),
+        { status: 400 }
+      );   
+}
+
+
+return new NextResponse(JSON.stringify(results) , {status:200})
+
+}catch(e:any){
+
+    return new NextResponse("Error in fetching users" +e.message , {status:500});
+
+}
+
+
+
+}
