acbd6bff73bf30b0200db576866bb27a46fbdac5
Uvindu Eshwarage <uvindu.nethmina30@gmail.com>
2025-10-21 18:36:51 +0530

    Successfully created battle request, battle, report user, report meme models

---
+++ lib/models/reportMeme.ts
@@ -0,0 +1,19 @@
+
+import {Schema,models,model} from "mongoose";
+
+const reportMemeSchema = new Schema(
+    {
+        user_id_reporter:{type:Schema.Types.ObjectId,ref:"User",required:true},
+        meme_id:{type:Schema.Types.ObjectId,ref:"Meme",required:true},
+        body:{type:String,required:true},
+        status:{type:String,required:true}   
+    },{
+
+    timestamps:true,
+
+}
+)
+
+const ReportMeme = models.ReportMeme || model("ReportMeme", reportMemeSchema)
+
+export default ReportMeme;
---
+++ lib/models/reportUser.ts
@@ -0,0 +1,16 @@
+import {Schema,model,models} from "mongoose";
+
+const reportUserSchema = new Schema (
+    {
+        user_id_to: {type:Schema.Types.ObjectId,ref:"User",required:true},
+        user_id_by: {type:Schema.Types.ObjectId,ref:"User",equired:true},
+        body: {type:String,required:true},
+
+    },
+    {
+        timestamps:true,
+    }
+)
+    const ReportUser = models.ReportUser || model("ReportUser",reportUserSchema);
+
+    export default ReportUser;
