bc8cbc70dd6d09b144f98e091c7e4ccbabf337ff
Uvindu Eshwarage <uvindu.nethmina30@gmail.com>
2025-10-21 18:58:18 +0530

    battle model implementation

--- lib/models/battle.ts
+++ lib/models/battle.ts
@@ -1,1 +1,20 @@
-import {Schema,models}
+import {Schema,models,model, SchemaType} from "mongoose";
+
+const battleSchema = new Schema(
+    {
+        user_id_by: {type:Schema.Types.ObjectId,ref:"User",required:true},
+        user_id_to: {type:Schema.Types.ObjectId,ref:"User",required:true},
+        vote_user_1: {type:Schema.Types.ObjectId,ref:"User",required: true},
+        vote_for_user_2: {type:Schema.Types.ObjectId,ref:"User",required: true},
+        startedTime: {type:Date,default: Date.now},
+        status: {type:String,required:true},
+        winner: {type:String,required:true},
+    },
+    {
+        timestamps:true
+    }
+);
+
+const Battle = models.Battle || model("Battle", battleSchema);
+
+export default Battle;
