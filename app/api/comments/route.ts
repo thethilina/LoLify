af4c39e0acc1affd3103b39999c582a823fff8a3
Thilina Dewmina <thilinadewmina7@gmail.com>
2025-10-23 20:40:17 +0530

    comment routes final

--- app/api/comments/route.ts
+++ app/api/comments/route.ts
@@ -8,6 +8,8 @@ import { Types } from "mongoose";
 
 export const GET = async (request : Request) => {
 
+ try{
+
 const {searchParams} =  new URL(request.url);
 const memeId = searchParams.get("memeId");
 
@@ -33,7 +35,7 @@ if(!meme){
 }
 
 const comments = await Comment.find(
-    {memeId}
+    {memeid :memeId}
 )
 
 if(comments.length === 0) {
@@ -48,10 +50,19 @@ if(comments.length === 0) {
  return new NextResponse(JSON.stringify(comments), { status: 200 });
 
 }
+catch(e:any){
+
+ return new NextResponse('Error getting meme' + e.message , {status:500})
+
+}
+
+
+}
 
 
 export const POST = async (request : Request) => {
 
+try{
 
 const {searchParams} =  new URL(request.url);
 const memeId = searchParams.get("memeId");
@@ -87,6 +98,140 @@ if(!user ){
 
 const body = await request.json();
 
+if(!body ){
+
+ return new NextResponse('Body  not found' , {status:404})
+}
+
+
 const newcomment = new Comment(body);
+
+await newcomment.save();
+
+if(!newcomment){
+    return new NextResponse("Error posting comment" , {status:400})
+}
+
+
+return new NextResponse(JSON.stringify({newcomment}) , {status:200})
+
+}catch(e:any){
+
+ return new NextResponse('Error posting comment'+ e.message , {status:500})
+
+}
+
+}
+
+
+
+export const PATCH = async (request : Request) => {
+
+try{
+
+const {searchParams} = new URL(request.url);
+const commentId = searchParams.get("commentId");
+const userId = searchParams.get("userId");
+
+if(!commentId || !userId){
+
+  return new NextResponse('comment id or user id not found' , {status:404})
+
+}
+
+if(!mongoose.Types.ObjectId.isValid(commentId) || !mongoose.Types.ObjectId.isValid(userId)){
+
+   return new NextResponse('comment id or User id is not valid' , {status:404})
+}
+
+
+await connect();
+
+const comment = await Comment.findById(commentId);
+const user = await User.findById(userId);
+
+if(!comment){
+
+ return new NextResponse('Meme  not found' , {status:400})
+}
+
+if(!user ){
+
+ return new NextResponse('User  not found' , {status:400})
+}
+
+
+const body = await request.json();
+
+if(!body){
+
+    return new NextResponse(JSON.stringify({message:"body not found"}) , {status:404})
+}
+
+const updatedcomment = await Comment.findByIdAndUpdate(
+    commentId,
+    {...body},
+    {new:true}
+)
+
+if(!updatedcomment){
+    return new NextResponse(JSON.stringify({message:"Error udating comment"}) , {status:400})
+}
+
+return new NextResponse( JSON.stringify({comment:updatedcomment}) , {status:200})
+
+}catch(e:any){
+
+ return new NextResponse('Error updating comment' + e.message, {status:500})
+
+
+}
+
+}
+
+export const DELETE = async (request :  Request) =>{
+
+try{
+
+ 
+const {searchParams} = new URL(request.url);
+const commentId = searchParams.get("commentId");
+
+
+if(!commentId ){
+
+  return new NextResponse('comment id or user id not found' , {status:404})
+
+}
+
+if(!mongoose.Types.ObjectId.isValid(commentId) ){
+
+   return new NextResponse('comment id or User id is not valid' , {status:404})
+}
+
+
+await connect();
+
+const comment = await Comment.findById(commentId);
+
+
+if(!comment){
+
+ return new NextResponse('Comment  not found' , {status:400})
+}
+   
+
+const deletedcomment = await Comment.findByIdAndDelete(commentId);
+
+if(!deletedcomment){
+    return new NextResponse(JSON.stringify({message:"Error deleting comment"}) , {status:400})
+}
+
+return new NextResponse(JSON.stringify({message:"Comment deleted successfully!"}) , {status:200})
+
+}catch(e:any){
+
+  return new NextResponse('Error deleting comment' + e.message, {status:500})   
+}
 
 }
