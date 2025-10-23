edca8de6bb2dcc1a90ab815e3ff7a04a8fc9cc57
Thilina Dewmina <thilinadewmina7@gmail.com>
2025-10-23 19:10:03 +0530

    search meme function

---
+++ app/api/memes/[search]/route.ts
@@ -0,0 +1,42 @@
+import { NextResponse } from "next/server";
+import connect from "@/lib/db";
+import Meme from "@/lib/models/meme";
+import { request } from "http";
+import mongoose from "mongoose";
+import { Types } from "mongoose";
+
+export const GET = async (request: Request, context: { params: any }) => {
+  const searchText = context.params?.search;
+
+  try {
+    if (!searchText || searchText.trim() === "") {
+      return new NextResponse(
+        JSON.stringify({ message: "Search text shouldn't be empty" }),
+        { status: 400 }
+      );
+    }
+
+    await connect();
+
+    const results = await Meme.find({
+      $or: [
+        { memetitle: { $regex: searchText, $options: "i" } },
+        { taglines: { $regex: searchText, $options: "i" } },
+      ],
+    });
+
+    if (!results || results.length === 0) {
+      return new NextResponse(
+        JSON.stringify({ message: "No meme found" }),
+        { status: 404 }
+      );
+    }
+
+    return new NextResponse(JSON.stringify(results), { status: 200 });
+  } catch (e: any) {
+    return new NextResponse(
+      JSON.stringify({ error: "Error searching memes", details: e.message }),
+      { status: 500 }
+    );
+  }
+};
