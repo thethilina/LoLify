d7a8c7d209effb8ba47e742784a463a9546a2b90
Thilina Dewmina <thilinadewmina7@gmail.com>
2025-10-24 10:07:56 +0530

    bitt

--- lib/models/FriendRequest.ts
+++ lib/models/FriendRequest.ts
@@ -3,7 +3,7 @@ import mongoose, {Schema , model , models} from "mongoose";
 const frequstSchema = new Schema (
 {
 
-    status : {type:String  , required : true },
+    
     byuserid :{type: mongoose.Schema.Types.ObjectId , ref: "User" ,required : true } ,
     touserid :{type: mongoose.Schema.Types.ObjectId , ref: "User" ,required : true } ,
 
