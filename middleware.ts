316d6feb2d0bd087fda9c89eed574e98fa760472
Thilina Dewmina <thilinadewmina7@gmail.com>
2025-10-25 19:59:53 +0530

    middleware

---
+++ middleware.ts
@@ -0,0 +1,28 @@
+
+import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+
+export async function middleware(req: NextRequest) {
+
+console.log("Current time:", Date.now() / 1000);
+
+  const token = req.cookies.get("token")?.value;
+  console.log("Token received in middleware:", token);
+  if (!token) return NextResponse.json({ message: "unauthorized" }, { status: 401 });
+
+  try {
+    const secret = process.env.JWT_SECRET!;
+    const decoded: any = jwt.verify(token, secret);
+
+    const res = NextResponse.next();
+    res.headers.set("loggeduserid", decoded.id); 
+    return res;
+  } catch {
+    return NextResponse.json({ message: "invalid or expired token" }, { status: 401 });
+  }
+}
+
+export const config = {
+  matcher: ["/api/comments/:path*", "/api/users/:path*" , "/api/memes/:path*" , "/api/friend_request/:path*" ], 
+   runtime: "nodejs",
+};
